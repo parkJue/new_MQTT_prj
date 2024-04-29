a01cadd56e813dbb63ff00ac9bdff0745b0bbc52
parkJue <pej6224@naver.com>
2024-04-29 23:30:58 +0900

    publish 수정

--- mqtt_app/publish.py
+++ mqtt_app/publish.py
@@ -54,6 +54,7 @@ def write_data():
         for value in list(pcs_data.values()) + list(bat_data.values()):
             packed_data = struct.pack('>f', value)
             registers = struct.unpack('>HH', packed_data)
+
             all_values.extend(registers)
 
 
@@ -62,25 +63,56 @@ def write_data():
 
         time.sleep(1)
 
+# def read_data():
+#     while True:
+#         all_data = modtc.read_holding_registers(0, len(pcs_data) + len(bat_data), unit=1)
+#         if not all_data.isError():
+#             # 읽은 데이터를 적절하게 할당
+#             pcs_values = all_data.registers[:len(pcs_data)]
+#             bat_values = all_data.registers[len(pcs_data):]
+
+#             for i, key in enumerate(pcs_data.keys()):
+#                 pcs_data[key] = pcs_values[i]
+#             for i, key in enumerate(bat_data.keys()):
+#                 bat_data[key] = bat_values[i]
+
+#         mqttc.publish('sensor/pcs', json.dumps(pcs_data))
+#         mqttc.publish('sensor/bat', json.dumps(bat_data))
+
+#         # print("PCS 데이터:", pcs_data)
+#         # print("BAT 데이터:", bat_data)
+
 def read_data():
     while True:
-        all_data = modtc.read_holding_registers(0, len(pcs_data) + len(bat_data), unit=1)
+        # 모든 PCS 및 BAT 데이터에 대한 레지스터 수 계산
+        num_registers = 2 * (len(pcs_data) + len(bat_data))  # 각 데이터 포인트에 2개 레지스터가 필요
+
+        # Modbus 레지스터에서 데이터 읽기
+        all_data = modtc.read_holding_registers(0, num_registers, unit=1)
         if not all_data.isError():
             # 읽은 데이터를 적절하게 할당
-            pcs_values = all_data.registers[:len(pcs_data)]
-            bat_values = all_data.registers[len(pcs_data):]
+            pcs_values = all_data.registers[:2*len(pcs_data)]
+            bat_values = all_data.registers[2*len(pcs_data):]
 
+            # PCS 데이터 변환
             for i, key in enumerate(pcs_data.keys()):
-                pcs_data[key] = pcs_values[i]
-            for i, key in enumerate(bat_data.keys()):
-                bat_data[key] = bat_values[i]
+                # 두 개의 16비트 정수를 바이트 데이터로 패킹
+                packed_data = struct.pack('>HH', pcs_values[2*i], pcs_values[2*i+1])
+                # 바이트 데이터를 부동소수점 값으로 언패킹
+                pcs_data[key], = struct.unpack('>f', packed_data)
 
+            # BAT 데이터 변환
+            for i, key in enumerate(bat_data.keys()):
+                packed_data = struct.pack('>HH', bat_values[2*i], bat_values[2*i+1])
+                bat_data[key], = struct.unpack('>f', packed_data)
+
+            # MQTT로 데이터 발행
         mqttc.publish('sensor/pcs', json.dumps(pcs_data))
         mqttc.publish('sensor/bat', json.dumps(bat_data))
 
-        # print("PCS 데이터:", pcs_data)
-        # print("BAT 데이터:", bat_data)
-        
+        # 주기적 업데이트를 위해 일시 중지
+        time.sleep(1)
+
 def main():
     try:
         write_thread = Thread(target=write_data)
